c760dfb03175dc6954082da704410e4aad16c6e9
fredricklitvin <fredlitvin@gmail.com>
2025-08-23 08:04:34 +0000

    added artifact main and output for 2 images main app and database

---
+++ modules/artifact/output.tf
@@ -0,0 +1,6 @@
+output "main_app_image_id" {
+    value = "google_artifact_registry_repository.main_app_image.id"
+}
+output "main_app_image_id" {
+    value = "google_artifact_registry_repository.main_app_image.id"
+}
