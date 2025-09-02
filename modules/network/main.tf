bdec7042f161c6bcb665af7222ed8e7fd26229f4
fredricklitvin <fredlitvin@gmail.com>
2025-09-02 16:00:55 +0000

    added variables to network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,41 +1,41 @@
 resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-network"
+  name                    = var.vpc_name
   auto_create_subnetworks = false
   }
 
 resource "google_compute_subnetwork" "private_subnet" {
-  name          = "private-subnet"
-  ip_cidr_range = "10.1.0.0/16"
-  region        = "us-central1"
+  name          = var.private_subnet_name
+  ip_cidr_range = var.private_subnet_cidr_range
+  region        = var.region
   network       = google_compute_network.vpc_network.id
   private_ip_google_access = true
   secondary_ip_range {
     range_name = "gke-pods"
-    ip_cidr_range = "10.3.0.0/16"
+    ip_cidr_range = var.private_subnet_cidr_range_pods
   }
     secondary_ip_range {
     range_name = "gke-services"
-    ip_cidr_range = "10.4.0.0/16"
+    ip_cidr_range = var.private_subnet_cidr_range_services
   }
   }
 
 resource "google_compute_subnetwork" "public_subnet" {
-  name          = "public-subnet"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-central1"
+  name          = var.public_subnet_name
+  ip_cidr_range = var.public_subnet_cidr_range
+  region        = var.region
   network       = google_compute_network.vpc_network.id
   }
 
 resource "google_compute_router" "router" {
-  name = "vpc-router"
-  region = "us-central1"
+  name = var.router_name
+  region = var.region
   network = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_router_nat" "nat" {
-  name = "nat"
+  name = var.nat_name
   router = google_compute_router.router.name
-  region = "us-central1"
+  region = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat  = "LIST_OF_SUBNETWORKS"
   subnetwork {
