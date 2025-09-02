06db33cb32e0b8561283b20618e1befbed3810e5
fredricklitvin <fredlitvin@gmail.com>
2025-09-02 20:22:53 +0000

    removed project var

--- variables.tf
+++ variables.tf
@@ -4,11 +4,11 @@ variable "region" {
   default     = "us-central1"
 }
 
-variable "project" {
-  description = "The GCP project being used"
-  type = string
-  sensitive   = true
-}
+# variable "project" {
+#   description = "The GCP project being used"
+#   type = string
+#   sensitive   = true
+# }
 
 #network mudule
 variable "vpc_name" {
